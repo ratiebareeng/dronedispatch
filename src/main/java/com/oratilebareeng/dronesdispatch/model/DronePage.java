41200b1ce5ef278d675cdde631a79f453c3c87de
ratie bareeng <bareengbiz@gmail.com>
2022-08-19 01:52:03 +0200

    Add DronePage model

---
+++ src/main/java/com/oratilebareeng/dronesdispatch/model/DronePage.java
@@ -0,0 +1,16 @@
+package com.oratilebareeng.dronesdispatch.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Sort;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DronePage {
+    private int pageNumber = 0;
+    private int pageSize = 5;
+    private Sort.Direction sortDirection = Sort.Direction.ASC;
+    private String sortBy = "serialNumber";
+}
