4be9327f236133506eeb245c14bd8c2cee6f972a
ratie bareeng <bareengbiz@gmail.com>
2022-08-19 01:08:25 +0200

    Add sample data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,25 @@
+INSERT INTO DRONE VALUES ('drone1', 75,'Lightweight',  'IDLE', 75.0); // <=100
+INSERT INTO Drone VALUES ('drone2', 96,'Lightweight',  'LOADING', 100.0);
+INSERT INTO Drone VALUES ('drone3', 45,'Middleweight',  'LOADED', 200.0); // <=250
+INSERT INTO Drone VALUES ('drone4', 60,'Middleweight',  'DELIVERING', 250.0);
+INSERT INTO Drone VALUES ('drone5', 87, 'Cruiserweight', 'DELIVERED', 295.0); // <=350
+INSERT INTO Drone VALUES ('drone6', 100,'Cruiserweight',  'RETURNING', 350.0);
+INSERT INTO Drone VALUES ('drone7',10, 'Cruiserweight',  'IDLE', 350.0);
+INSERT INTO Drone VALUES ('drone8',  24,'Heavyweight', 'IDLE', 400.0); // <= 500
+INSERT INTO Drone VALUES ('drone9', 33,'Heavyweight',  'IDLE', 450.0);
+INSERT INTO Drone VALUES ('drone10', 52, 'Heavyweight', 'IDLE', 500.0);
+
+INSERT INTO Medication (id, code, image, name, weight) VALUES (1,'med1', 'med1_image.jpg', 'Albuterol', 25);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (2,'med2', 'med2_image.jpg', 'Amlodipine', 10);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (3,'med3', 'med3_image.jpg', 'Gabapentin', 300);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (4,'med4', 'med4_image.jpg', 'Hydrochlorothiazide', 10);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (5,'med5', 'med5_image.jpg', 'Levothyroxine', 75);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (6,'med6', 'med6_image.jpg', 'Lipitor', 10);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (7,'med7', 'med7_image.jpg', 'Lisinopril', 10);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (8, 'med8', 'med8_image.jpg', 'Losartan', 50);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (9, 'med9', 'med9_image.jpg', 'Metformin', 500);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (10,'med10', 'med10_image.jpg', 'Metoprolol', 200);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (11,'med11', 'med11_image.jpg', 'Norvasc-Amlodipine', 5);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (12,'med12', 'med12_image.jpg', 'Omeprazole', 20);
+INSERT INTO Medication (id, code, image, name, weight) VALUES (13,'med13', 'med13_image.jpg', 'Zoloft', 50);
+
