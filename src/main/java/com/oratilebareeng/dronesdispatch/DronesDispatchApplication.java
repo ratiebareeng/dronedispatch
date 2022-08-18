67838887bf32d9dd573c76c265b077289edf0e97
ratie bareeng <bareengbiz@gmail.com>
2022-08-19 00:11:54 +0200

    Project Setup and Configuration

---
+++ src/main/java/com/oratilebareeng/dronesdispatch/DronesDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.oratilebareeng.dronesdispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oratilebareeng/dronesdispatch/DronesDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oratilebareeng.dronesdispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
