082fdfa8e23fc8de1d3cdd27e4bcb0d8b62e460c
ratie bareeng <bareengbiz@gmail.com>
2022-08-19 11:32:04 +0200

    Update: Use medication code as ID/Primary Key

--- src/main/java/com/oratilebareeng/dronesdispatch/model/Medication.java
+++ src/main/java/com/oratilebareeng/dronesdispatch/model/Medication.java
@@ -14,19 +14,9 @@ import javax.persistence.*;
 @EqualsAndHashCode
 public class Medication {
     @Id
-    @SequenceGenerator(
-            name = "medication_id_sequence",
-            sequenceName = "medication_id_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "medication_id_sequence"
-    )
-    private final Long id;
-    private final String name;
-    private final String code;
-    private final String image; // todo: implement image upload, update
-    private final int weight;
+    private String code;
+    private String name;
+    private String image; // todo: implement image upload, update
+    private int weight;
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,17 +9,17 @@ INSERT INTO Drone VALUES ('drone8',  24,'Heavyweight', 'IDLE', 400.0); // <= 500
 INSERT INTO Drone VALUES ('drone9', 33,'Heavyweight',  'IDLE', 450.0);
 INSERT INTO Drone VALUES ('drone10', 52, 'Heavyweight', 'IDLE', 500.0);
 
-INSERT INTO Medication (id, code, image, name, weight) VALUES (1,'med1', 'med1_image.jpg', 'Albuterol', 25);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (2,'med2', 'med2_image.jpg', 'Amlodipine', 10);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (3,'med3', 'med3_image.jpg', 'Gabapentin', 300);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (4,'med4', 'med4_image.jpg', 'Hydrochlorothiazide', 10);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (5,'med5', 'med5_image.jpg', 'Levothyroxine', 75);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (6,'med6', 'med6_image.jpg', 'Lipitor', 10);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (7,'med7', 'med7_image.jpg', 'Lisinopril', 10);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (8, 'med8', 'med8_image.jpg', 'Losartan', 50);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (9, 'med9', 'med9_image.jpg', 'Metformin', 500);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (10,'med10', 'med10_image.jpg', 'Metoprolol', 200);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (11,'med11', 'med11_image.jpg', 'Norvasc-Amlodipine', 5);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (12,'med12', 'med12_image.jpg', 'Omeprazole', 20);
-INSERT INTO Medication (id, code, image, name, weight) VALUES (13,'med13', 'med13_image.jpg', 'Zoloft', 50);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med1', 'med1_image.jpg', 'Albuterol', 25);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med2', 'med2_image.jpg', 'Amlodipine', 10);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med3', 'med3_image.jpg', 'Gabapentin', 300);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med4', 'med4_image.jpg', 'Hydrochlorothiazide', 10);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med5', 'med5_image.jpg', 'Levothyroxine', 75);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med6', 'med6_image.jpg', 'Lipitor', 10);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med7', 'med7_image.jpg', 'Lisinopril', 10);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med8', 'med8_image.jpg', 'Losartan', 50);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med9', 'med9_image.jpg', 'Metformin', 500);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med10', 'med10_image.jpg', 'Metoprolol', 200);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med11', 'med11_image.jpg', 'Norvasc-Amlodipine', 5);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med12', 'med12_image.jpg', 'Omeprazole', 20);
+INSERT INTO Medication (code, image, name, weight) VALUES ('med13', 'med13_image.jpg', 'Zoloft', 50);
 
